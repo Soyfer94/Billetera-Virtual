5e25830839c3c83456ccca6735c46b86ebb29045
Soyfer94 <acostafernando94@hotmail.com>
2023-07-20 23:01:50 -0300

    primeros cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Navbar from './componentes/barra';
 import Eventos from './componentes/contador';
 
 const App = () => {
     return (
     <div className="App">
-      <h1>Hola REACT</h1>
       
+      <Navbar/>
       <Eventos/>
+      <h2>Hola REACT</h2>
     </div>
     );
 }
---
+++ src/componentes/barra.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './barra.scss';
+
+const Navbar = () => {
+  return (
+    <header>
+    <div class="container">
+    <p class="logo">TuUNSTA</p>
+    <nav>
+        <a href="">Bancos asociados</a>
+        <a href="">Cripto</a>
+        <a href="">Promociones</a>
+    </nav>
+</div>
+</header>
+  );
+};
+
+export default Navbar;
