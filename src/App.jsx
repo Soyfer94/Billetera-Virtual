e759fbf4cd3a95d867ce758776dfa334145cf9bd
fersanchez1993 <fernandos1993@outlook.com>
2023-07-23 19:28:49 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import './App.css'
+<<<<<<< Updated upstream
+=======
+import CrudApp from './componentes/CrudApp';
+import Formulario from './componentes/Formulario';
+import FormularioControlado from './componentes/FormularioControlado';
+>>>>>>> Stashed changes
 import Navbar from './componentes/barra';
 import Portada from './componentes/portada';
 import Cards from './componentes/cards';
@@ -11,7 +17,19 @@ const App = () => {
       
       <Navbar/>
       <Portada/>
+<<<<<<< Updated upstream
       <Cards/>
+=======
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Formulario/>
+      <FormularioControlado/>
+      <CrudApp/> "(no tocar)"
+      
+      
+>>>>>>> Stashed changes
     </div>
     );
 }
