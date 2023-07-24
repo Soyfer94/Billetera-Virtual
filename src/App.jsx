592584f2460869338942729dfb334c2edab85777
Soyfer94 <acostafernando94@hotmail.com>
2023-07-24 16:43:37 -0300

    API cripto

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import './App.css'
-import CryptoTable from './componentes/API';
 
 import Crypto from './componentes/Crypto';
-
 import Formulario from './componentes/Formulario';
 import FormularioControlado from './componentes/FormularioControlado';
 import Navbar from './componentes/barra';
 import Card from './componentes/card';
 import Portada from './componentes/portada';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import TableCoins from './componentes/APItable';
 
 
 function App ()  {
+
+      const [coins, setCoins] = useState([]);
+      const [search, setSearch] = useState("");
+    
+      const getData = async () => {
+        try {
+          const res = await axios.get(
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+          );
+          setCoins(res.data);
+          console.log(res.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    
+      useEffect(() => {
+        getData();
+      }, []);
+    
+
+
+
+
+
     return (
     <div className="App">
       
@@ -23,7 +49,19 @@ function App ()  {
       <h1 className='text-center'>React - Crypto</h1>
       <Crypto/>
 
-      <CryptoTable/>
+     <div className="container">
+      <div className="row">
+        <input
+          type="text"
+          placeholder='Busca la moneda'
+          className="form-control bg-dark text-light border-0 mt-4 text-center"
+          autoFocus
+          onChange={(e) => setSearch(e.target.value)}
+        />
+            
+        <TableCoins coins={coins} search={search} />
+      </div>
+    </div>
       
     </div>
     );
@@ -31,10 +69,3 @@ function App ()  {
 
 export default App; 
 
-/*
-import Form from './componentes/CriptoForm';
-import CrudApp from './componentes/CrudApp';
-
-<Form/> "(no tocar)"
-      <CrudApp/> "(no tocar)"
-      */
